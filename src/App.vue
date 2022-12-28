22c30d1c94ce501139c7a965c11cb04e9142b0ce
潘国庆 <panguoqing@intra.nsfocus.com>
2022-12-28 18:05:18 +0800

    测试vscode提交2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home111</router-link> |
+    <router-link to="/">Home11122</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
