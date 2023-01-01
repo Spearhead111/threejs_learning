044d8eeec7b5a42cd02020b09f77dffe9cf0cd63
Spearhead111 <18101524079@163.com>
2023-01-02 00:10:37 +0800

    清除body的margin和padding

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <nav>
+  <nav v-if="!$route.meta.fullScreen">
     <router-link to="/CombinedType_1">CombinedType_1</router-link> | <router-link to="/CombinedType_2">CombinedType_2</router-link> |
     <router-link to="/Test3D">Test3D</router-link>
   </nav>
   <router-view />
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const $route = useRoute();
+onMounted(() => {
+  console.log($route.meta.fullScreen);
+});
+</script>
+
+<style lang="less">
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
   // margin-top 60px
+}
 </style>
