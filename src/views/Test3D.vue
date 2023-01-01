1d59afb1dd4b557700c1a3b41d5846785b2a9ba9
Spearhead111 <18101524079@163.com>
2023-01-02 00:52:37 +0800

    将渲染场景的手动更新优化到页面改变事件上

--- src/views/Test3D.vue
+++ src/views/Test3D.vue
@@ -2,19 +2,18 @@
  * @Author: Spearhead
  * @Date: 2022-12-31 21:42:12
  * @LastEditors: Spearhead
- * @LastEditTime: 2023-01-02 00:08:10
+ * @LastEditTime: 2023-01-02 00:51:46
 -->
 <template>
   <div class="control">
     <button @click="addScene">添加</button>
     <button @click="removeScene">清除</button>
-    <button @click="updateScene">刷新</button>
   </div>
   <div id="my-three"></div>
 </template>
 
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted } from 'vue';
+import { onBeforeUnmount } from 'vue';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
@@ -33,13 +32,11 @@ let light: any; // 点光源
 let renderer: any; // WebGL渲染器
 let controls: any; // 轨道控制器
 let axesHelper: any; // 辅助坐标轴
-// let width = 1000,
-//   height = 500;
 let width = window.innerWidth,
   height = window.innerHeight;
 let raycaster: any;
 let doAnimation = false; // 是否进行animate
-let stats: any;
+let stats: any; // 性能控件
 let renderPass: any; // 场景通道
 let outlinePass: any; // 物体边缘发光通道
 let composer: any; // EffectComposer（效果组合器）对象
@@ -48,7 +45,6 @@ let composer: any; // EffectComposer（效果组合器）对象
 const initScene = () => {
   //创建一个三维场景
   scene = new THREE.Scene();
-
   //创建一个物体（形状）
   const geometry = new THREE.BoxGeometry(100, 100, 100); //长宽高都是100的立方体
   // const geometry = new THREE.SphereGeometry(60,40,40);//半径是60的圆
@@ -70,7 +66,6 @@ const initScene = () => {
     color: 0xffff00,
   });
   mesh2 = new THREE.Mesh(geometry2, material2); //网格模型对象Mesh
-  // mesh3.translateX(120); //球体网格模型沿Y轴正方向平移120
   mesh2.position.set(120, 0, 0); //设置mesh3模型对象的xyz坐标为120,0,0
   scene.add(mesh2);
   //添加光源 //会照亮场景里的全部物体（氛围灯），前提是物体是可以接受灯光的，这种灯是无方向的，即不会有阴影。
@@ -79,9 +74,7 @@ const initScene = () => {
   light.position.set(120, 0, 400); // 设置点光源位置
   scene.add(ambient);
   scene.add(light);
-  //创建一个透视相机，窗口宽度，窗口高度
-  // const width = window.innerWidth,
-  //   height = window.innerHeight;
+  //创建一个透视相机
   camera = new THREE.PerspectiveCamera(45, width / height, 10, 5000);
   //设置相机位置
   camera.position.set(300, 300, 300);
@@ -94,7 +87,7 @@ const initScene = () => {
   //创建辅助坐标轴
   axesHelper = new THREE.AxesHelper(1000); //参数200标示坐标系大小，可以根据场景大小去设置
   scene.add(axesHelper);
-  //创建一个WebGL渲染器
+  // 创建一个WebGL渲染器
   // renderer = new THREE.WebGLRenderer();
   // 设置渲染器的透明度实现背景
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
@@ -146,11 +139,19 @@ const removeWindowResizeEvent = () => {
   window.removeEventListener('resize', onWindowResize);
 };
 
-//  页面大小改变回调
+//  页面大小改变回调,根据页面的大小更新更新渲染
 const onWindowResize = () => {
   width = window.innerWidth;
   height = window.innerHeight;
-  renderer && renderer.setSize(width, height);
+  // 更新渲染区域大小
+  renderer.setSize(width, height);
+  // // css2d渲染器更新
+  // scene.cssRender.setSize(width, height);
+  // // css3d渲染器更新
+  // scene.css3DRender.setSize(width, height);
+  // 更新相机
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
 };
 
 // 点击事件
@@ -163,7 +164,6 @@ const clickEvent = () => {
   // 注意这里的xy,如果是渲染在整个页面用clientXY,如果渲染在容器里建议用offsetXY,不然会有偏差
   mouse.x = (event.offsetX / width) * 2 - 1;
   mouse.y = -(event.offsetY / height) * 2 + 1;
-
   raycaster.setFromCamera(mouse, camera);
   let intersects = raycaster.intersectObjects(scene.children);
   if (intersects && intersects.length > 0) {
@@ -225,12 +225,6 @@ const addScene = () => {
   }
 };
 
-// 更新场景
-const updateScene = () => {
-  removeScene();
-  addScene();
-};
-
 // 移除场景
 const removeScene = () => {
   removeClickEvent();
@@ -241,16 +235,6 @@ const removeScene = () => {
   composer = null;
   renderer = null;
   scene = null;
-  console.log(composer);
-  console.log(renderer);
-  console.log(scene);
-  console.log(camera);
-  console.log(mesh);
-  console.log(ambient);
-  console.log(light);
-  console.log(axesHelper);
-  console.log(controls);
-  console.log(raycaster);
 };
 
 // 清除render
@@ -258,7 +242,6 @@ const releaseRender = (renderer: any, scene: any) => {
   if (!renderer || !scene || document.getElementById('my-three')?.childElementCount == 0) {
     return;
   }
-
   renderer.domElement && document.getElementById('my-three')?.removeChild(renderer.domElement);
   stats?.domElement && document.getElementById('my-three')?.removeChild(stats.domElement);
   let clearScene = function (scene: any) {
@@ -297,10 +280,6 @@ const releaseRender = (renderer: any, scene: any) => {
 
   THREE.Cache.clear();
 };
-
-onMounted(() => {
-  console.log(width, height);
-});
 
 onBeforeUnmount(() => {
   console.log('准备组件卸载前清除缓存和节点');
